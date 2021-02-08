c1ece9578434f9bd0ccd4eccf1d8a858aaf4e429
Drew Bradley <williamandrewb@gmail.com>
2021-02-07 18:13:46 -0700

    Exercise.

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Switch, Route, NavLink} from "react-router-dom"
 import puppies from '../data/puppy-data.js';
 import sharks from '../data/shark-data.js';
 import Creatures from '../Creatures/Creatures';
+import CreatureDetails from '../Creatures/CreatureDetails'
 import Home from '../Home/Home';
 
 export default class App extends Component {
@@ -10,10 +12,21 @@ export default class App extends Component {
     return (
       <main className="App">
         <nav>
-          <a href="/puppies" className="nav">Puppies</a>
-          <a href="/sharks" className="nav">Sharks</a>
+          <NavLink to="/puppies" className="nav">Puppies</NavLink>
+          <NavLink to="/sharks" className="nav">Shart</NavLink>
         </nav>
-        <h1>Puppies or Sharks?</h1>
+        <h1>Puppies or Shart?</h1>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/puppies" 
+            render={() => <Creatures name="puppies" data={puppies} />} />
+        <Route path="/sharks" 
+            render={() => <Creatures name="sharks" data={sharks} />} />
+        <Route path="/puppies/:id" exact 
+            render={({match}) => {
+              const { id } = match.params;
+              const creatureToRender = puppies.find(creature => creature.id === parseInt(id))
+              return <CreatureDetails {...creatureToRender} />
+              }} />
       </main>
     );
   }
